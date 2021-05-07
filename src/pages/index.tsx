a49d08b71059c3f9a569b1a6aff2ba0e481d3ca8
Henrique Teles Barbosa <henrique_telesbarbosa@hotmail.com>
2021-05-07 01:35:21 -0300

    nextjs basic

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+            <title>ig.news</title>
+      </Head>
+      <h1 className={styles.title}> 
+        Hello World
+      </h1>
+    </>
+  )
+}
